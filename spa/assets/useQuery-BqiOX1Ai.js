8594e8cdd2698b9cfa56bd9628a98b3461a34fc9
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-01-27 19:49:29 +0000

    deploy: e24f2163dbfb8064f6f639059e604d888907fc33

--- spa/assets/useQuery-BqiOX1Ai.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./errors-o8k4PwST.js";function a(e,s){return r(e,u)}export{a as u};
---
+++ spa/assets/useQuery-DM1K8Od6.js
@@ -0,0 +1,1 @@
+import{u as r,Q as u}from"./errors-BxqVUl8u.js";function a(e,s){return r(e,u)}export{a as u};
