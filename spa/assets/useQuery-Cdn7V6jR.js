383e85a41305c9841e3e09ee6828dc0b551b6715
barsukovasonya1 <barsukovasonya1@users.noreply.github.com>
2026-01-27 00:36:18 +0000

    deploy: fd543deeaa454d1a1b7681d8865ec081b85c8f2a

---
+++ spa/assets/useQuery-Cdn7V6jR.js
@@ -0,0 +1,1 @@
+import{u as r,Q as u}from"./errors-Di30m1S1.js";function a(e,s){return r(e,u)}export{a as u};
--- spa/assets/useQuery-DcAuCxu8.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./errors-C9-KVE9g.js";function a(e,s){return r(e,u)}export{a as u};
