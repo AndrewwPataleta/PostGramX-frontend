95a6e089776536f71752d8979549935785a0d869
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-01-27 19:36:30 +0000

    deploy: fa1cf88582eec2ec080cd940a7d3fe860f75588d

--- spa/assets/useQuery-BMygtY2j.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./errors-C-z1UHmJ.js";function a(e,s){return r(e,u)}export{a as u};
---
+++ spa/assets/useQuery-BqiOX1Ai.js
@@ -0,0 +1,1 @@
+import{u as r,Q as u}from"./errors-o8k4PwST.js";function a(e,s){return r(e,u)}export{a as u};
