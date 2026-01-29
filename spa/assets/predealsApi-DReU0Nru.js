828992174698583f48ec30637d989358a5505298
barsukovasonya1 <barsukovasonya1@users.noreply.github.com>
2026-01-29 23:27:08 +0000

    deploy: ecfac4563056eac54f315ee7b8157434000967e1

--- spa/assets/predealsApi-DReU0Nru.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index-DHmd0Tb8.js";const r=async e=>a("/predeals/create",e),t=async e=>a("/predeals/get",e),p=async e=>{await a("/predeals/cancel",e)};export{p as a,t as b,r as p};
