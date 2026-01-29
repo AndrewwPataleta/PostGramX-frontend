303107b0461aaf1c7a012ccca3b2459d38b8202d
barsukovasonya1 <barsukovasonya1@users.noreply.github.com>
2026-01-29 20:16:11 +0000

    deploy: 096ae2bdf1fd273fa34264d056c60d9996a66a15

--- spa/assets/listingsApi-C6VJ8hL_.js
+++
@@ -1,1 +0,0 @@
-import{p as n}from"./index-BRaUjsT1.js";const a=async s=>n("/listings/create",s),i=async s=>n("/listings/by-channel",s);export{a as c,i as l};
---
+++ spa/assets/predealsApi-DoKa8Bnp.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index-53kr_7Yp.js";const r=async e=>a("/predeals/create",e),t=async e=>a("/predeals/get",e),p=async e=>{await a("/predeals/cancel",e)};export{p as a,t as b,r as p};
