66eb83c476d97714b3de2d85f2cbced61a0553dd
barsukovasonya1 <barsukovasonya1@users.noreply.github.com>
2026-01-30 19:13:45 +0000

    deploy: d5c17f8090f7fb9c9b38013ebdd952346b259916

---
+++ spa/assets/predealsApi-DOC99oQe.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index-CA2fCERY.js";const r=async e=>a("/predeals/create",e),t=async e=>a("/predeals/get",e),p=async e=>{await a("/predeals/cancel",e)};export{p as a,t as b,r as p};
